473a055191826703daf0a8db82973dbed979cd65
sembry <skembry@hotmail.com>
2020-04-08 18:52:13 -0700

    add play button functionality

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+  public bool isPlay;
+  public bool isInstructions;
+
+  void OnMouseUp(){
+  	if(isPlay)
+  	{
+  		Application.LoadLevel(1);
+  	}
+  	// if(isInstructions)
+  	// {
+  	// 	Application.Quit();
+  	// }
+  } 
+}
