36cb162bdbdedc192d13954005c66dff34d540ef
sembry <skembry@hotmail.com>
2020-04-13 00:50:04 -0700

    add play, restart, and main menu functionality

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pausePanel;
+    public Button playButton;
+    public Button restartButton;
+    public Button mainMenuButton;
 
     // Start is called before the first frame update
     void Start()
     {
       Time.timeScale = 1;
       pausePanel.SetActive(false);
+      //adding onClick functionality to menu buttons
+      Button play = playButton.GetComponent<Button>();
+      play.onClick.AddListener(PlayOnClick);
+
+      Button restart = restartButton.GetComponent<Button>();
+      restart.onClick.AddListener(RestartOnClick);
+
+      Button mainMenu = mainMenuButton.GetComponent<Button>();
+      mainMenu.onClick.AddListener(MenuOnClick);
     }
 
     // Update is called once per frame
@@ -31,5 +45,26 @@ public class PauseMenu : MonoBehaviour
             pausePanel.SetActive(false);
           }
         }
+    }
+
+    void PlayOnClick()
+    {
+      // check to ensure pause menu is shown
+      if(pausePanel.activeInHierarchy)
+      {
+        // resume game
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+      }
+    }
+
+    void RestartOnClick()
+    {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void MenuOnClick()
+    {
+      SceneManager.LoadScene(0);
     }
 }
