f100be583a2c4bc3a35244b90612e77396786d43
sembry <skembry@hotmail.com>
2020-04-21 01:31:07 -0700

    add instructions skeleton

---
+++ Assets/Scripts/Instructions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Instructions : MonoBehaviour
+{
+    void OnMouseUp()
+    {
+      SceneManager.LoadScene(0);
+      Debug.Log("hi");
+    }
+}
