2495262b424cda702cf24cb7da0727b8fe505422
sembry <skembry@hotmail.com>
2020-04-21 20:22:45 -0700

    add level selection functionality

---
+++ Assets/Scripts/Menus/LevelSelector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    public bool isLevel1;
+    public bool isLevel2;
+    public bool isLevel3;
+    public bool isLevel4;
+    public bool isLevel5;
+    public bool isBack;
+
+    void OnMouseUp(){
+      if(isLevel1)
+      {
+        SceneManager.LoadScene("Level1");
+      }
+    	if(isLevel2)
+    	{
+    		SceneManager.LoadScene("Level2");
+    	}
+      if(isLevel3)
+    	{
+    		SceneManager.LoadScene("Level3");
+    	}
+      if(isLevel4)
+    	{
+    		SceneManager.LoadScene("Level4");
+    	}
+      if(isLevel5)
+    	{
+    		SceneManager.LoadScene("Level5");
+    	}
+      if(isBack)
+      {
+        SceneManager.LoadScene("MainMenu");
+      }
+    }
+}
